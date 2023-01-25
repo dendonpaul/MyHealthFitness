b441d2cbdd1a8f23b55a3a992ac97d3e24e165bf
dendonpaul <dendonpaul@gmail.com>
2023-01-25 20:57:27 +0530

    updated header sticky function

--- script.js
+++ script.js
@@ -7,3 +7,18 @@ var swiper = new Swiper(".swiper", {
     el: ".swiper-pagination",
   },
 });
+
+//sticky scroll effect
+window.onscroll = function () {
+  stickyFunction();
+};
+const stickyFunction = () => {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    document.querySelector(".header").className = "header sticky-header";
+  } else {
+    document.querySelector(".header").className = "header";
+  }
+};
