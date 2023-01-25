6cc604acfa78996ce03c717eddf8cc31dc5bebea
dendonpaul <dendonpaul@gmail.com>
2023-01-26 01:14:49 +0530

    features section added

--- script.js
+++ script.js
@@ -18,7 +18,28 @@ const stickyFunction = () => {
     document.documentElement.scrollTop > 100
   ) {
     document.querySelector(".header").className = "header sticky-header";
+    // document.getElementsByClassName("header")[0].className =
+    //   "header sticky-header";
   } else {
     document.querySelector(".header").className = "header";
   }
 };
+
+//Features section.
+let overlay = document.querySelectorAll(".overlay");
+
+for (let i = 0; i <= overlay.length - 1; i++) {
+  //visible event.
+  overlay[i].addEventListener("mouseover", (e) => {
+    e.target.parentNode.children[3].style.visibility = "visible";
+
+    e.target.parentNode.children[3].addEventListener("mouseover", () => {
+      e.target.parentNode.children[3].style.visibility = "visible";
+    });
+  });
+
+  //Hide event.
+  overlay[i].addEventListener("mouseout", (e) => {
+    e.target.parentNode.children[3].style.visibility = "hidden";
+  });
+}
